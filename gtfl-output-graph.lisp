17c7b4d105d8db22e45c93bdeeee8b630dc56de7
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-08-20 16:56:03 +0200

    /bin/bash

--- gtfl-output-graph.lisp
+++ gtfl-output-graph.lisp
@@ -20,7 +20,7 @@
     (to-dot graph :stream stream)
     :close-stream
     (multiple-value-bind (output error-output exit-code)
-        (uiop:run-program (list "/usr/bin/bash"
+        (uiop:run-program (list "/bin/bash"
                                 "-c"
                                 "set -o pipefail ; dot -Tsvg -Grankdir=LR | sed -e '1,3d'")
                           :input pathname
