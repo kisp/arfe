81f79e48377698be26fda085991cc6c6dd202d39
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-08-21 05:03:00 +0200

    #+sbcl(:import-from :sb-ext #:quit #:exit)

--- package.lisp
+++ package.lisp
@@ -1,10 +1,23 @@
 ;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; Coding:utf-8 -*-
 
 (defpackage :arfe
-  (:use :common-lisp :alexandria :argsem-soundness
-   :graph-adj :trivial-graph-canonization :graph-apx :graph-tgf
-   :graph :graph-dot :gtfl
-        :lparallel
-   :arfe.generate-non-isomorphic :arfe.dc-ds-eq :arfe.good-example
-        :arfe.gtfl-output-graph :arfe.directg)
+  (:use
+   :alexandria
+   :arfe.dc-ds-eq
+   :arfe.directg
+   :arfe.generate-non-isomorphic
+   :arfe.good-example
+   :arfe.gtfl-output-graph
+   :argsem-soundness
+   :common-lisp
+   :graph
+   :graph-adj
+   :graph-apx
+   :graph-dot
+   :graph-tgf
+   :gtfl
+   :lparallel
+   :trivial-graph-canonization
+   )
+  #+sbcl(:import-from :sb-ext #:quit #:exit)
   (:export))
